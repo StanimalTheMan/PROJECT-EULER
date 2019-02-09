d4fd5e55bbad46ebe56dfe734b8cab7c51fec5ab
StanimalTheMan <sjc591@nyu.edu>
2019-02-08 20:45:13 -0500

    Euler the Great  himself  helped me improve my code.

--- 001_multiples_of_3_and_5.py
+++ 001_multiples_of_3_and_5.py
@@ -5,9 +5,13 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 '''
 
 def sum_of_natural_multiples_of_three_or_five():
+    ''' NOOB WAY
+
     sum = 0 # running sum
     qualified_numbers = [x for x in range(1000) if x % 5  == 0 or x % 3 == 0]
     for num in qualified_numbers:
         sum += num
     return sum
-
+    '''
+    qualified_numbers = [x for x in range(1000) if x % 5  == 0 or x % 3 == 0]
+    return sum(qualified_numbers)
